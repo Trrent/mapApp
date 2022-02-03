925984d501f609a65abc9edcdb444fde72b20f62
KPEHDEJIb <94686039+KPEHDEJIb@users.noreply.github.com>
2022-02-03 18:44:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Window(QWidget, WindowForm):
         super().__init__()
         self.setGeometry(500, 250, *SCREEN_SIZE)
         self.coords = [37.530887, 55.70311]
+        self.zoom = 17
         self.setupUi(self)
         self.updateMap()
 
@@ -23,11 +24,19 @@ class Window(QWidget, WindowForm):
             self.coords[0] += 0.00693
         elif event.key() == Qt.Key_Left:
             self.coords[0] -= 0.00693
+        elif event.key() == Qt.Key_PageUp and self.zoom < 17:
+            self.zoom += 1
+            if self.zoom == 2:
+                self.coords = [37.530887, 55.70311]
+        elif event.key() == Qt.Key_PageDown and self.zoom > 1:
+            self.zoom -= 1
+            if self.zoom == 1:
+                self.coords = [0.0, 0.0]
 
         self.updateMap()
 
     def updateMap(self):
-        map_img = QByteArray(get_image(' '.join(map(str, self.coords)), 17))
+        map_img = QByteArray(get_image(' '.join(map(str, self.coords)), self.zoom))
         self.pixmap.loadFromData(map_img)
         self.image.setPixmap(self.pixmap)
 
@@ -37,4 +46,3 @@ if __name__ == '__main__':
     window = Window()
     window.show()
     sys.exit(app.exec())
-
